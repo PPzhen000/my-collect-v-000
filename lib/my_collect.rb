5a0bc6c6d37ff3a173a3121ea9070ec33ee22baa
Ariel Zhen <zhenpingping@live.com>
2018-05-27 07:29:08 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,3 +5,13 @@ def my_collect(array)
     i += 1
   end
 end
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
