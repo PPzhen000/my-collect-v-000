2777b815d8dd7b48c20749a330eaaf42a79bd8a5
Ariel Zhen <zhenpingping@live.com>
2018-05-27 07:32:47 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,18 +1,9 @@
 def my_collect(array)
   i = 0
+  collection = [] 
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
 end
 
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-collection = []
-my_collect(array) do |name|
-  collection << name.split(" ").first
-end
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  collection << lang.upcase
-end
